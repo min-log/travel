545bbac48d5892d66bed958733e92325d59ac166
jimin <minlog.0430@gmail.com>
2023-04-19 01:29:26 +0900

    item 생성 팝업 추가

--- src/main/java/com/example/travel/domain/Category.java
+++ src/main/java/com/example/travel/domain/Category.java
@@ -1,5 +1,6 @@
 package com.example.travel.domain;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,24 +11,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Getter
 @Entity
-public class User {
-
+public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long user_no;
-
-    private String email;
-    private Long nickname;
-    private String password;
-    private String name;
-
-    private String userPhone;
-    private String userAddress;
-    private String picture;
+    private Long category_id;
+    private String category_name;
 }
