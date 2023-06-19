ef45ffa0507ae03de629cd244946a1fddc2788ce
jimin <minlog.0430@gmail.com>
2023-06-19 18:46:07 +0900

    230619 오류 수정

--- src/main/java/com/example/travel/repository/ImageRepository.java
+++ src/main/java/com/example/travel/repository/ImageRepository.java
@@ -2,7 +2,9 @@ package com.example.travel.repository;
 
 import com.example.travel.domain.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image,Long> {
 
 }
--- src/main/java/com/example/travel/repository/ItemRepository.java
+++ src/main/java/com/example/travel/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.example.travel.repository;
 
 import com.example.travel.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item,Long> {
 }
